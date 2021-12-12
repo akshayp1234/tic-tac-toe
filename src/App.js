b5d7151ce1564b2c424516ec74eb4a08605f4b64
Akshay Patil <akp204@gmail.com>
2021-12-12 22:16:23 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Section1 from './Section1.js'
+function App() {
+  
+  return (
+    <div className="App">
+      
+      <Section1/>
+    </div>
+  );
+}
+
+export default App;
